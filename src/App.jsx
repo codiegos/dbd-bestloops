6a2f1b413bf47137ce339970b55badc7a699ae0e
Diego Rivera <diegosebsk@gmail.com>
2023-03-06 01:48:30 -0300

    add background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { Maps } from './components/Maps'
+import { maps } from './data/maps'
+
 function App () {
   return (
-    <div className='flex flex-col items-center text-white bg-gradient-to-b from-slate-800 to-slate-900 min-h-screen'>App</div>
+    <>
+      <div className='flex flex-col items-center text-white min-h-screen bg-dbd bg-cover'>
+        <h1 className='pt-20 text-3xl'>Dead by daylight Best Loops</h1>
+        <ul className='grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-4 pt-20'>
+          <Maps data={maps} />
+        </ul>
+      </div>
+    </>
   )
 }
 
