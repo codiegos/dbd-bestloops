0d926608b2e2159cb5a4a8d01e8e0fd87ebadaf7
DiegoTroncosoJara <diego.troncosoojara@gmail.com>
2023-03-06 02:54:30 -0300

    Add and show All images in a Grid

---
+++ src/assets/images/index.js
@@ -0,0 +1,79 @@
+import Asy_Asylum from './iconMap_Asy_Asylum.png'
+import Asy_Chapel from './iconMap_Asy_Chapel.png'
+import Brl_MaHouse from './iconMap_Brl_MaHouse.png'
+import Brl_Template from './iconMap_Brl_Temple.png'
+import Ecl_Level01 from './iconMap_Ecl_Level01.png'
+import Eng_ElmStreet from './iconMap_Eng_ElmStreet.png'
+import Eng_ElmStreet02 from './iconMap_Eng_ElmStreet02.png'
+import Eng_ElmStreet03 from './iconMap_Eng_ElmStreet03.png'
+import Eng_ElmStreet04 from './iconMap_Eng_ElmStreet04.png'
+import Eng_ElmStreet05 from './iconMap_Eng_ElmStreet05.png'
+import Fin_TheGame from './iconMap_Fin_TheGame.png'
+import Frn_Barn from './iconMap_Frm_Barn.png'
+import Frm_Cornfield from './iconMap_Frm_Cornfield.png'
+import Frm_Farmhouse from './iconMap_Frm_Farmhouse.png'
+import Frm_Silo from './iconMap_Frm_Silo.png'
+import Frm_Slaughterhouse from './iconMap_Frm_Slaughterhouse.png'
+import Hos_Treatment from './iconMap_Hos_Treatment.png'
+import Hti_Manor from './iconMap_Hti_Manor.png'
+import Hti_Shrine from './iconMap_Hti_Shrine.png'
+import Ind_CoalTower from './iconMap_Ind_CoalTower.png'
+import Ind_Forest from './iconMap_Ind_Forest.png'
+import Ind_Foundry from './iconMap_Ind_Foundry.png'
+import Ind_Mine from './iconMap_Ind_Mine.png'
+import Ind_Storehouse from './iconMap_Ind_Storehouse.png'
+import Ion_Level01 from './iconMap_Ion_Level01.png'
+import Jnk_Garage from './iconMap_Jnk_Garage.png'
+import Jnk_GasStation from './iconMap_Jnk_GasStation.png'
+import Jnk_Lodge from './iconMap_Jnk_Lodge.png'
+import Jnk_Office from './iconMap_Jnk_Office.png'
+import Jnk_Scrapyard from './iconMap_Jnk_Scrapyard.png'
+import Kny_Cottage from './iconMap_Kny_Cottage.png'
+import Sub_Street from './iconMap_Sub_Street.png'
+import Swp_GrimPantry from './iconMap_Swp_GrimPantry.png'
+import Swp_PaleRose from './iconMap_Swp_PaleRose.png'
+import Ukr_Saloon from './iconMap_Ukr_Saloon.png'
+import Wal_Level01 from './iconMap_Wal_Level01.png'
+
+
+
+
+
+export {
+    Asy_Asylum,
+    Asy_Chapel,
+    Brl_MaHouse,
+    Brl_Template,
+    Ecl_Level01,
+    Eng_ElmStreet,
+    Eng_ElmStreet02,
+    Eng_ElmStreet03,
+    Eng_ElmStreet04,
+    Eng_ElmStreet05,
+    Fin_TheGame,
+    Frn_Barn,
+    Frm_Cornfield,
+    Frm_Farmhouse,
+    Frm_Silo,
+    Frm_Slaughterhouse,
+    Hos_Treatment,
+    Hti_Manor,
+    Hti_Shrine,
+    Ind_CoalTower,
+    Ind_Forest,
+    Ind_Foundry,
+    Ind_Mine,
+    Ind_Storehouse,
+    Ion_Level01,
+    Jnk_Garage,
+    Jnk_GasStation,
+    Jnk_Lodge,
+    Jnk_Office,
+    Jnk_Scrapyard,
+    Kny_Cottage,
+    Sub_Street,
+    Swp_GrimPantry,
+    Swp_PaleRose,
+    Ukr_Saloon,
+    Wal_Level01
+};
--- src/data/maps.js
+++ src/data/maps.js
@@ -1,31 +1,292 @@
+import {Asy_Asylum,
+  Asy_Chapel,
+  Brl_MaHouse,
+  Brl_Template,
+  Ecl_Level01,
+  Eng_ElmStreet,
+  Eng_ElmStreet02,
+  Eng_ElmStreet03,
+  Eng_ElmStreet04,
+  Eng_ElmStreet05,
+  Fin_TheGame,
+  Frn_Barn,
+  Frm_Cornfield,
+  Frm_Farmhouse,
+  Frm_Silo,
+  Frm_Slaughterhouse,
+  Hos_Treatment,
+  Hti_Manor,
+  Hti_Shrine,
+  Ind_CoalTower,
+  Ind_Forest,
+  Ind_Foundry,
+  Ind_Mine,
+  Ind_Storehouse,
+  Ion_Level01,
+  Jnk_Garage,
+  Jnk_GasStation,
+  Jnk_Lodge,
+  Jnk_Office,
+  Jnk_Scrapyard,
+  Kny_Cottage,
+  Sub_Street,
+  Swp_GrimPantry,
+  Swp_PaleRose,
+  Ukr_Saloon,
+  Wal_Level01} from '../assets/images/index'
+
 export const maps = [
   {
     id: 1,
-    title: 'Ironworks of Misery',
+    title: 'Asylum',
     description: '',
-    category: 'The MacMillan Estate',
-    urlImage: 'https://media.discordapp.net/attachments/917649484450775061/936776046613766184/iconMap_Ind_Foundry.png'
+    category: 'Asylum',
+    urlImage: Asy_Asylum
   },
   {
     id: 2,
-    title: 'El Cuadrado Roto',
+    title: 'Asylum Chapel',
     description: '',
-    category: 'The MacMillan Estate',
-    urlImage: 'https://cdn.discordapp.com/attachments/917649484450775061/936776021666070599/iconMap_Ind_CoalTower.png'
+    category: '',
+    urlImage: Asy_Chapel
   },
   {
     id: 3,
-    title: 'Suffocation Pit',
+    title: 'Brl_MaHouse',
     description: '',
-    category: 'The MacMillan Estate',
-    urlImage: 'https://cdn.discordapp.com/attachments/917649484450775061/936776047024816128/iconMap_Ind_Mine.png'
+    category: '',
+    urlImage: Brl_MaHouse
   },
   {
     id: 4,
-    title: 'El Cuadrado Roto',
-    description: 'desc 2',
+    title: 'Brl_Template',
+    description: '',
     category: '',
-    urlImage: 'https://cdn.discordapp.com/attachments/917649484450775061/936776021666070599/iconMap_Ind_CoalTower.png'
+    urlImage: Brl_Template
+  },
+  {
+    id: 5,
+    title: 'Ecl_Level01',
+    description: '',
+    category: '',
+    urlImage: Ecl_Level01
+  },
+  {
+    id: 6,
+    title: 'Eng_ElmStreet',
+    description: '',
+    category: '',
+    urlImage: Eng_ElmStreet
+  },
+  {
+    id: 7,
+    title: 'Eng_ElmStreet02',
+    description: '',
+    category: '',
+    urlImage: Eng_ElmStreet02
+  },
+  {
+    id: 8,
+    title: 'Eng_ElmStreet03',
+    description: '',
+    category: '',
+    urlImage: Eng_ElmStreet03
+  },
+  {
+    id: 9,
+    title: 'Eng_ElmStreet04',
+    description: '',
+    category: '',
+    urlImage: Eng_ElmStreet04
+  },
+  {
+    id: 10,
+    title: 'Eng_ElmStreet05',
+    description: '',
+    category: '',
+    urlImage: Eng_ElmStreet05
+  },
+  {
+    id: 11,
+    title: 'Fin_TheGame',
+    description: '',
+    category: '',
+    urlImage: Fin_TheGame
+  },
+  {
+    id: 12,
+    title: 'Frn_Barn',
+    description: '',
+    category: '',
+    urlImage: Frn_Barn
+  },
+  {
+    id: 13,
+    title: 'Frm_Cornfield',
+    description: '',
+    category: '',
+    urlImage: Frm_Cornfield
+  },
+  {
+    id: 14,
+    title: 'Frm_Farmhouse',
+    description: '',
+    category: '',
+    urlImage: Frm_Farmhouse
+  },
+  {
+    id: 15,
+    title: 'Frm_Silo',
+    description: '',
+    category: '',
+    urlImage: Frm_Silo
+  },
+  {
+    id: 16,
+    title: 'Frm_Slaughterhouse',
+    description: '',
+    category: '',
+    urlImage: Frm_Slaughterhouse
+  },
+  {
+    id: 17,
+    title: 'Hos_Treatment',
+    description: '',
+    category: '',
+    urlImage: Hos_Treatment
+  },
+  {
+    id: 18,
+    title: 'Hti_Manor',
+    description: '',
+    category: '',
+    urlImage: Hti_Manor
+  },
+  {
+    id: 19,
+    title: 'Hti_Shrine',
+    description: '',
+    category: '',
+    urlImage: Hti_Shrine
+  },
+  {
+    id: 20,
+    title: 'Ind_CoalTower',
+    description: '',
+    category: '',
+    urlImage: Ind_CoalTower
+  },
+  {
+    id: 21,
+    title: 'Ind_Forest',
+    description: '',
+    category: '',
+    urlImage: Ind_Forest
+  },
+  {
+    id: 22,
+    title: 'Ind_Foundry',
+    description: '',
+    category: '',
+    urlImage: Ind_Foundry
+  },
+  {
+    id: 23,
+    title: 'Ind_Mine',
+    description: '',
+    category: '',
+    urlImage: Ind_Mine
+  },
+  {
+    id: 24,
+    title: 'Ind_Storehouse',
+    description: '',
+    category: '',
+    urlImage: Ind_Storehouse
+  },
+  {
+    id: 25,
+    title: 'Ion_Level01',
+    description: '',
+    category: '',
+    urlImage: Ion_Level01
+  },
+  {
+    id: 26,
+    title: 'Jnk_Garage',
+    description: '',
+    category: '',
+    urlImage: Jnk_Garage
+  },
+  {
+    id: 27,
+    title: 'Jnk_GasStation',
+    description: '',
+    category: '',
+    urlImage: Jnk_GasStation
+  },
+  {
+    id: 28,
+    title: 'Jnk_Lodge',
+    description: '',
+    category: '',
+    urlImage: Jnk_Lodge
+  },
+  {
+    id: 29,
+    title: 'Jnk_Office',
+    description: '',
+    category: '',
+    urlImage: Jnk_Office
+  },
+  {
+    id: 30,
+    title: 'Jnk_Scrapyard',
+    description: '',
+    category: '',
+    urlImage: Jnk_Scrapyard
+  },
+  {
+    id: 31,
+    title: 'Kny_Cottage',
+    description: '',
+    category: '',
+    urlImage: Kny_Cottage
+  },
+  {
+    id: 32,
+    title: 'Sub_Street',
+    description: '',
+    category: '',
+    urlImage: Sub_Street
+  },
+  {
+    id: 33,
+    title: 'Swp_GrimPantry',
+    description: '',
+    category: '',
+    urlImage: Swp_GrimPantry
+  },
+  {
+    id: 34,
+    title: 'Swp_PaleRose',
+    description: '',
+    category: '',
+    urlImage: Swp_PaleRose
+  },
+  {
+    id: 35,
+    title: 'Ukr_Saloon',
+    description: '',
+    category: '',
+    urlImage: Ukr_Saloon
+  },
+  {
+    id: 36,
+    title: 'Wal_Level01',
+    description: '',
+    category: '',
+    urlImage: Wal_Level01
   }
-
+  
 ]
