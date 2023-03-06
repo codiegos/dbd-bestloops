2a12c2a4f94338073a1dfa6f4f9b2c3d40ca8f1e
Diego Rivera <diegosebsk@gmail.com>
2023-03-05 23:01:15 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+function App () {
+  return (
+    <div className='flex flex-col items-center text-white bg-gradient-to-b from-slate-800 to-slate-900 min-h-screen'>App</div>
+  )
+}
+
+export default App
